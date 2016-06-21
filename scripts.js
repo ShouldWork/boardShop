9daa7bec7d3095ef4e1112375491ecb5d24a627c
Krysp <mrblocke@gmail.com>
2016-06-21 10:03:48 -0600

    Updated page and JS

--- scripts.js
+++ scripts.js
@@ -1,6 +1,37 @@
+var list = "<ul id='sizeList'><li>30</li><li>40</li><li>50</li><li>60</li></ul>";
+
+
 $(document).ready(function(){
-	var forSaleItems = ($("#forSale").find("ul"));
-	forSaleItems.find("li").each(function( index ){
-		 $(this).addClass("itemCell");
-	});
+	popTiles();
+	clickTiles();
 });
+
+
+
+
+
+
+
+function popTiles(){
+	var forSaleItems = ($(".tileContainer").find("ul"));
+	forSaleItems.addClass("addFlex_center");
+	forSaleItems.find("li").each(function(){
+		$(this).on("click",function(){
+			clickTiles(this);
+		});
+		$(this).addClass("tile-item");
+		$(this).on("mouseenter",function(){
+			$(this).addClass("highlighted");
+			$(this).css("background-color","rgba(0,0,0,0.90)");
+		});
+		$(this).on("mouseleave",function(){
+			$(this).removeClass("highlighted");
+			$(this).css("background-color","rgba(0,0,0,0.70)");
+		})
+	});
+}
+
+function clickTiles(tile){
+	$(this).toggleClass("addFlex_center");
+	$(tile).append(list);
+}
